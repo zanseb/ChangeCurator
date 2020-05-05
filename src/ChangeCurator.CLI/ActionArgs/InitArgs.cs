44fa656110a9d7dbbdb7a17536077bb3b3de5e57
Sebastian Zanoni <zanonisebastian@gmail.com>
2020-05-05 13:17:52 +0200

    Define commands which shall be supported

--- src/ChangeCurator.CLI/ActionArgs/InitArgs.cs
+++ src/ChangeCurator.CLI/ActionArgs/InitArgs.cs
@@ -5,5 +5,13 @@ namespace ChangeCurator.CLI.ActionArgs
     [Verb("init", HelpText = "Initialize the project to keep track of")]
     class InitArgs
     {
+        [Option('p', "project", Required = true, HelpText = "The name of the project")]
+        public string ProjectName { get; set; }
+
+        [Option('d', "directory", Required = true, HelpText = "The root of the directory")]
+        public string FilePath { get; set; }
+
+        [Option('u', "url", HelpText = "The url which has to be prefixed to the issue identifier")]
+        public string IssueUrl { get; set; }
     }
 }
---
+++ src/ChangeCurator.SDK/EntryType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeCurator.SDK
+{
+    public enum EntryType
+    {
+        Added,
+        Changed,
+        Deprecated,
+        Removed,
+        Fixed,
+        Security,
+    }
+}
